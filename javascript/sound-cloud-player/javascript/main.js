bcafe18ffb208be68099cf59c6140886580b2a95
kimsj9484 <kimsj9484@gmail.com>
2018-11-06 09:47:56 +0900

    set API

--- javascript/sound-cloud-player/javascript/main.js
+++ javascript/sound-cloud-player/javascript/main.js
@@ -1,17 +1,25 @@
 /* 1. 검색 */
 
-/* 2. SoundCloud API  사용하기 */
+/* 2. SoundCloud API  사용하기 : Object 안에 key, value 형태로 구축한다.*/
+const SoundCloudAPI = {
+  init: () => {
+    SC.initialize({
+      client_id: "cd9be64eeb32d1741c17cb39e41d254d"
+    });
+  },
 
-SC.initialize({
-  client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
-});
+  getTrack: (_track) => {
+    SC.get("/tracks", {
+      q: _track
+    }).then(function(_tracks) {
+      console.log(_tracks);
+    });
+  },
+};
 
-// find all sounds of buskers licensed under 'creative commons share alike'
-SC.get('/tracks', {
-  q: 'buskers', license: 'cc-by-sa'
-}).then(function(tracks) {
-  console.log(tracks);
-});
+SoundCloudAPI.init();
+SoundCloudAPI.getTrack('coffee');
+
 /* 3. 카드 보여주기 */
 
 /* 4. Playlist 에 추가하고 실제로 재생하기 */
